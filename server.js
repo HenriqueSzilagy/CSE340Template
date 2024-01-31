7d2a5bab3e5518b315239c3ecb596848e033d09e
HenriqueSzilagy <henriqueszilagy@gmail.com>
2024-01-30 21:36:49 -0300

    up

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
 const utilities = require("./utilities/");
+const bodyParser = require("body-parser")
+
 
 /* ***********************
  * Middleware
@@ -41,6 +43,9 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
